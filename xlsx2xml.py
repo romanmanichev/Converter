19c90b434eec08bc1305faa0c0dde5e5363eedb8
romanmanichev <134366485+romanmanichev@users.noreply.github.com>
2023-12-24 17:33:35 +0300

    Update xlsx2xml.py

--- xlsx2xml.py
+++ xlsx2xml.py
@@ -1,106 +1,101 @@
+import datetime
+
 from openpyxl import load_workbook
 from lxml import etree
 import datetime as dt
+import re
+from pandas import ExcelFile
+
+
+# Функция получения названии листов excel файла
+def get_list_from_excel_file(fileEXCEL):
+    return ExcelFile(fileEXCEL).sheet_names
 
 
 # Фильтрация через единый формальный параметр функции (дата, снилс, полис)
-def xlsx2xml(fileEXCEL, fileXML, tags, NameOfList='Лист1', FilerOfData=True):
+def xlsx2xml(fileEXCEL, fileXML, tags, NameOfErrorLogFile, NameOfList='Лист1', FilerOfData=False):
+    # Загрузка excel файла и листа
+    wb = load_workbook(f"{fileEXCEL}")
+    sheet = wb[f"{NameOfList}"]
 
-	# Загрузка excel файла и листа
-	wb = load_workbook(f"{fileEXCEL}.xlsx")
-	sheet = wb[f"{NameOfList}"]
+    # Создание <ZL_LIST> тега
+    ZL_LIST = etree.Element("ZL_LIST")
 
-	# Создание <ZL_LIST> тега
-	ZL_LIST = etree.Element("ZL_LIST")
+    # Формирование тега <ZGLV>
+    ZGLV = etree.SubElement(ZL_LIST, "ZGLV")
+    filename = etree.SubElement(ZGLV, "FILENAME")
+    data = etree.SubElement(ZGLV, "DATA")
+    code_mo = etree.SubElement(ZGLV, "CODE_MO")
+    year = etree.SubElement(ZGLV, "YEAR")
+    r = etree.SubElement(ZGLV, "R")
 
-	# Формирование тега <ZGLV>
-	ZGLV = etree.SubElement(ZL_LIST, "ZGLV")
-	filename = etree.SubElement(ZGLV, "FILENAME")
-	data = etree.SubElement(ZGLV, "DATA")
-	code_mo = etree.SubElement(ZGLV, "CODE_MO")
-	year = etree.SubElement(ZGLV, "YEAR")
-	r = etree.SubElement(ZGLV, "R")
+    # заполнение тега <ZGLV>
+    filename.text = fileXML
+    data.text = str(dt.date.today())
+    code_mo.text = '352506'
+    year.text = str(dt.date.today().year)
+    r.text = '1'
 
-	# заполнение тега <ZGLV>
-	filename.text = fileXML
-	data.text = '2023-01-01'
-	code_mo.text = '352506'
-	year.text = str(dt.date.today().year)
-	r.text = '1'
+    # Счетчик для идентифиактора N_ZAP
+    counter = 1
 
-	# Счетчик для идентифиактора N_ZAP
-	counter = 1
+    # Функция для проверки даты по фильтру
+    def check_date(olddate, filter):
+        try:
+            return dt.datetime.strptime(olddate, filter).date()
+        except:
+            return False
 
-	# Функция для проверки даты по фильтру
-	def check_date(olddate, filter):
-		try: return dt.datetime.strptime(olddate, filter).date()
-		except: return False
-	
+    # Перебор excel файла
+    for i in range(1, sheet.max_row + 1):
 
-	# Перебор excel файла
-	for i in range(1, sheet.max_row+1):
+        # Создание <ZAP> тега
+        column = etree.SubElement(ZL_LIST, "ZAP")
 
-		# Создание <ZAP> тега
-		column = etree.SubElement(ZL_LIST, "ZAP")
+        # Счетчик колонки
+        colCounter = 1
 
-		# Добавление идентификатора N_ZAP
-		column2 = etree.SubElement(column, "N_ZAP")
-		column2.text = str(counter)
-		counter += 1
+        # Получение итерируемой строки
+        for tag in tags:
+            if re.fullmatch(r'\(\D{0,}\)', tag) != None:
+                column1 = etree.SubElement(column, tag.strip('(').strip(')'))
+                column1.text = ""
+            elif tag == "N_ZAP":
+                # Добавление идентификатора N_ZAP
+                column2 = etree.SubElement(column, "N_ZAP")
+                column2.text = str(counter)
+                counter += 1
+            else:
+                column3 = etree.SubElement(column, tag)
+                # Условия для проверки некоторых тегов
+                otherColumn = str(sheet.cell(row=i, column=colCounter).value)
+                if check_date(otherColumn, '%Y-%m-%d %H:%M:%S') != False:
+                    column3.text = str(check_date(otherColumn, '%Y-%m-%d %H:%M:%S'))
+                elif check_date(otherColumn.strip(' '), '%d.%m.%Y') != False:
+                        column3.text = str(check_date(otherColumn.strip(' '), '%d.%m.%Y'))
+                else:
+                    # Даты, которые не попали под фильтрацию
+                    column3.text = otherColumn.strip(' ')
 
-		# Получение итерируемой строки
-		for j, k in zip(range(1, sheet.max_column+1), range(1, len(tags))): # Первый элемент range является номером столбца, с которого надо считывать данные
+                # Проверка включенного фильтра
+                if FilerOfData == True:
 
-			# Условия для проверки некоторых тегов
-			if tags[k] == "DR":
-				# Преобразование даты в следующую маску ГГГГ-ММ-ДД
-				olddate = str(sheet.cell(row=i, column=j).value)
+                    # Идентификация тега ENP и SNILS
+                    if tag == 'ENP':
 
-				# Формирование тега из переменной tags
-				column1 = etree.SubElement(column, tags[k])
+                        # Проверка тега ENP на длинну 16-ти символов
+                        if len(str(sheet.cell(row=i, column=colCounter).value)) != 16:
+                            with open(f"{NameOfErrorLogFile}.txt", 'a', encoding='UTF-8') as file:
+                                file.write(f"Номер записи {i} Столбец ENP: не содержит 16 цифр\n")
+                    elif tag == 'SNILS':
+                        # Проверка тега SNLIS на совпадение шаблону
+                        match = re.fullmatch(r"\d{3}-\d{3}-\d{3} \d{2}", str(sheet.cell(row=i, column=colCounter).value))
+                        if match == None:
+                            with open(f"{NameOfErrorLogFile}.txt", 'a', encoding='UTF-8') as file:
+                                file.write(f"Номер записи {i} Столбец SNILS: не совпадает шаблону\n")
 
-				# Фильтрация даты по фильтрамы
-				if check_date(olddate, '%Y-%m-%d %H:%M:%S') != False:
-					column1.text = str(check_date(olddate, '%Y-%m-%d %H:%M:%S'))
-				elif check_date(olddate.strip(' '), '%d.%m.%Y') != False:
-					column1.text = str(check_date(olddate.strip(' '), '%d.%m.%Y'))
-				else:
-					# Даты, которые не попали под фильтрацию
-					column1.text = olddate
-			
-			elif tags[k] == "PHONE":
-				# Формирование тега из переменной tags
-				column1 = etree.SubElement(column, tags[k])
+                colCounter += 1
 
-				# Добавление соответствующего текста в тег
-				column1.text = str('')
-			elif tags[k] == "ENP":
-				if FilerOfData != False:
-					if len(str(sheet.cell(row=i, column=j).value)) != 16:
-						with open('errorLog.txt', 'a', encoding='UTF-8') as f:
-							f.write(f'Строка {i} стробец ENP не соответствует количеству символов\n')
-			else:
-				# Формирование тега из переменной tags
-				column1 = etree.SubElement(column, tags[k])
-
-				# Добавление соответствующего текста в тег
-				if sheet.cell(row=i, column=j).value == None:
-					column1.text = ""
-				else:
-					column1.text = str(sheet.cell(row=i, column=j).value)
-
-		# Ввод в xml файл доп.тегов
-		column2 = etree.SubElement(column, "MDR")
-		column2.text = str(2)
-		column3 = etree.SubElement(column, "ADDRESDP")
-		column3.text = str("г.Вологда, ул.Окружное ш., д.3в")
-		column4 = etree.SubElement(column, "DISP_TYP")
-		column4.text = str(4)
-		column5 = etree.SubElement(column, "DATADP")
-		column5.text = str("2023-11-20")
-		column6 = etree.SubElement(column, "TIMEDP")
-		column6.text = str("09:00:00")
-
-	# Создание дерева и xml файла
-	tree = etree.ElementTree(ZL_LIST)
-	tree.write(f"{fileXML}.xml", pretty_print=True, encoding='windows-1251')
+    # Создание дерева и xml файла
+    tree = etree.ElementTree(ZL_LIST)
+    tree.write(f"{fileXML}.xml", pretty_print=True, encoding='windows-1251')
